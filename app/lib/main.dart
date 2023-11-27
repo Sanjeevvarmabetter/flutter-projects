77cc13b9c15418bdeeda41b8cb128d885c498867
Sanjeevvarmabetter <sanjeevvarmacode@gmail.com>
2023-11-28 02:18:25 +0530

    -

---
+++ app/lib/main.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 190, 18, 187),
+        body: Center(
+          child: Text('Hello Sanjeev'),
+        ),
+      ),
+    ),
+  );
+}
+
+
