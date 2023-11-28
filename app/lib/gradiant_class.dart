85f9d3e918209c79eb9e090fb8e61814607c9b32
Sanjeevvarmabetter <sanjeevvarmacode@gmail.com>
2023-11-28 14:24:50 +0530

    images not showing

--- app/lib/gradiant_class.dart
+++ app/lib/gradiant_class.dart
@@ -1,28 +1,51 @@
 import 'package:app/text_styles.dart';
 import 'package:flutter/material.dart';
 
-
 //this class is performing inheritance
 class GradientContainer extends StatelessWidget {
   //this is a constructor
-  const GradientContainer({super.key}); 
-  
+
+  const GradientContainer({super.key});
+  var activeDiceImage = 'assets/images/Dice-2.png';
+  rollDice() {
+    activeDiceImage = 'assets/images/Dice-4.png';
+    print('Changing image ....')
+  }
+
   @override
   Widget build(context) {
     return Container(
       decoration: const BoxDecoration(
         gradient: LinearGradient(
           colors: [
-            Color.fromARGB(211, 100, 31, 99),
-            Color.fromARGB(212, 54, 6, 54)
+            Color.fromARGB(210, 0, 149, 248),
+            Color.fromARGB(211, 225, 4, 225)
           ],
           begin: Alignment.topLeft,
           end: Alignment.bottomRight,
         ),
       ),
-      child: const Center(
-        child: Textstyle(),
-      ),
+      child: Center(
+          child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/Dice-2.png',
+            width: 200,
+          ),
+          const SizedBox(height: 40),
+          TextButton(
+            onPressed: rollDice,
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.white,
+              textStyle: TextStyle(
+                fontSize: 29,
+              ),
+            ),
+            child: Text('Roll Dice'),
+          )
+        ],
+      )),
     );
   }
 }
